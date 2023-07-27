7f3085237b8a2218812c15141055f592991d7346
Kshitij Gupta <84291907+darklord-2900@users.noreply.github.com>
2023-07-27 23:22:37 +0530

    modified

--- josephusprob.java
+++ josephusprob.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
+
 //recursion problem 
 public class josephusprob {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         try (Scanner ab = new Scanner(System.in)) {
-            int n=ab.nextInt();
-            int k=ab.nextInt();
+            int n = ab.nextInt();
+            int k = ab.nextInt();
             josephusprob ob = new josephusprob();
-           int res= ob.prob(n,k);
-           System.out.println("winner is: "+res);
+            int res = ob.prob(n, k);
+            System.out.println("winner is: " + res);
         }
-     }
-    int prob(int n,int k){
-        if(n==1){
+    }
+
+    int prob(int n, int k) {
+        if (n == 1) {
             return 0;
         }
-        System.out.println(n+" ");
-        return (prob(n-1, k)+k)%n;
+        System.out.println(n + " ");
+        return (prob(n - 1, k) + k) % n;
     }
 }
